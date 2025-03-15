24e9fe5821be4ddc0aa2f8bef42f4b60286fd0b2
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2025-03-15 13:14:53 -0300

    feat: add custom 404 page

--- src/static/custom.js
+++ src/static/custom.js
@@ -1,1 +1,6 @@
-/* Customize me */
+document.addEventListener("DOMContentLoaded", function () {
+    if (document.title.includes("404")) {
+        const primeiraDiv = document.querySelector("div");
+        primeiraDiv.classList.add("main-404");
+    }
+});
